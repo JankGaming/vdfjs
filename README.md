b127bd4432e06329ea3eecf70d13c385e8223adc
Michael Barrett <twisterghost@gmail.com>
2015-07-13 21:00:46 -0400

    Initial commit of code

---
+++ README.md
@@ -0,0 +1,24 @@
+# VDF.js
+
+Convert Valve's KeyValue format to JSON
+
+## Install
+
+`npm install vdfjs`
+
+## Usage
+
+### In a script
+
+```javascript
+var vdf = require('vdfjs');
+
+var jsonString = vdf.parse(someVDFfile);
+```
+
+### CLI
+`node vdf.js path_to_file`
+
+## Note
+
+This repo is a work in progress. Expect updated...everything...soon.
---
+++ vdf.js
@@ -0,0 +1,48 @@
+var fs = require('fs');
+var _ = require('lodash');
+
+var currentLocation = [];
+var obj = {};
+
+var levelRegex = /^"([A-Za-z0-9\_\-]*)"$/;
+var propertyRegex = /^"([A-Za-z1-9_\-]*)"[\t\ ]+"(.*)"$/;
+
+function parseLine(line) {
+
+  line = line.trim();
+  line = line.replace(/\/\/.+/, '');
+  var match;
+
+  if (match = line.match(levelRegex)) {
+    currentLocation.push(match[1]);
+  } else if (match = line.match(propertyRegex)) {
+    if (match[2] === null) {
+      console.log(match);
+    }
+    _.set(obj, currentLocation.join('.') + '.' + match[1], match[2]);
+  }
+
+  if (line === '}') {
+    currentLocation.pop();
+  }
+
+}
+
+function parse(vdf) {
+  obj = {};
+
+  var lines = vdf.split('\n');
+
+  _.forEach(lines, function(line) {
+    parseLine(line);
+  });
+
+  return JSON.stringify(obj);
+}
+
+exports.parse = parse;
+
+if (require.main === module) {
+  var fileData = fs.readFileSync(process.argv[2], 'utf-8');
+  fs.writeFileSync('out.json', parse(fileData));
+}
