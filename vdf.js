dfb40ce38c8d0ad33420e88d8aa8cfbaf69415ac
Michael Barrett <twisterghost@gmail.com>
2016-02-21 17:27:38 -0500

    Support object arrays

--- vdf.js
+++ vdf.js
@@ -9,11 +9,25 @@ var propertyRegex = /^"([A-Za-z1-9_\-]*)"[\t\ ]+"(.*)"$/;
 function parseLine(line) {
 
   line = line.trim();
-  line = line.replace(/\/\/.+/, '');
+
+  line = stripComments(line);
   var match;
 
   if (match = line.match(levelRegex)) {
-    currentLocation.push(match[1]);
+    var matchedKey = match[1];
+    var nextLocation = _.clone(currentLocation);
+    nextLocation.push(matchedKey);
+    nextLocation = nextLocation.join('.');
+    var checkKey = _.get(obj, nextLocation);
+    if (checkKey && !_.isArray(checkKey)) {
+      convertToArray(nextLocation);
+      currentLocation.push(matchedKey + '[1]');
+    } else if (checkKey && _.isArray(checkKey)){
+      nextLocationLength = _.get(obj, nextLocation).length;
+      currentLocation.push(matchedKey + '[' + nextLocationLength + ']');
+    } else {
+      currentLocation.push(match[1]);
+    }
   } else if (match = line.match(propertyRegex)) {
     _.set(obj, currentLocation.join('.') + '.' + match[1], match[2]);
   }
@@ -22,6 +36,15 @@ function parseLine(line) {
     currentLocation.pop();
   }
 
+}
+
+function convertToArray(location) {
+  var currentValue = _.get(obj, location);
+  _.set(obj, location, [currentValue]);
+}
+
+function stripComments(line) {
+  return line.replace(/\/\/.+/, '');
 }
 
 function parse(vdf) {
